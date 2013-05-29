4cffa21c23f61b8dbe2ce6049767b00658e0ade2
Jared A. Kirschner <jared@jaredkirschner.com>
2013-05-28 19:29:47 -0700

    Added basic routing structure.

--- app.js
+++ app.js
@@ -5,22 +5,23 @@
 
 var express = require('express'),
 	routes = require('./routes'),
-	user = require('./routes/user'),
+	cats = require('./routes/cats'),
 	http = require('http'),
 	path = require('path');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+	app.set('port', process.env.PORT || 3000); // sets up the port
+	app.set('views', path.join(__dirname, 'views')); // sets the path for views
+	app.set('view engine', 'jade'); // sets the engine that the views are rendered with
+	app.use(express.favicon()); // default favicon
+	app.use(express.logger('dev')); // error logging
+	app.use(express.bodyParser()); // 
+	app.use(express.methodOverride());
+	app.use(app.router); // 
+	app.use(express.static(path.join(__dirname, 'public'))); // sets the path for public files (css & js)
+});
 
 // development only
 if ('development' == app.get('env')) {
@@ -28,8 +29,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/cats/new', routes.new_cat);
-app.get('/users', user.list);
+app.get('/cats', cats.index);
+app.get('/cats/new', cats.add);
+app.get('/cats/color/:color', cats.by_color);
+app.get('/cats/delete/old', cats.delete_oldest);
 
 /*
 Create an app that has the following routes:
@@ -39,7 +42,8 @@ Create an app that has the following routes:
     GET /cats => shows a sorted list of cats by age. This should display
     * their names, colors, and age
     GET /cats/color/:color => where :color is a parameter, such as 
-    * "orange" or "grey". It shows a sorted list of cats by age that have that specific color
+    * "orange" or "grey". It shows a sorted list of cats by age that 
+    * have that specific color
     GET /cats/delete/old => deletes the oldest cat :c The cat should no
     * longer appear on any lists
 */
---
+++ routes/cats.js
@@ -0,0 +1,20 @@
+
+/*
+ * GET cat related pages.
+ */
+
+exports.index = function(req, res){
+	res.send("placeholder - cat index");
+}
+
+exports.add = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.by_color = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.delete_oldest = function(req, res){
+  res.send("respond with a resource");
+};
